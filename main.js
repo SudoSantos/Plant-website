ac2e820670dddfe06a22ed4d7b4a3f8456f605e2
kanu Chukwuemeka <mekuseo@gmail.com>
2024-11-26 12:47:46 +0100

    completed review section

--- main.js
+++ main.js
@@ -13,3 +13,44 @@ navLink.forEach(link => {
     hamburger.classList.toggle("ri-close-large-line");
   })
 })
+
+// SHOW SCROLL UP
+
+
+// CHANGE BACKGROUND HEADER
+
+
+// SWIPER
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+});
+
+
+// SCROLL SECTIONS ACTIVE LINK
+
+
+// SCROLL REVEAL ANIMATION
